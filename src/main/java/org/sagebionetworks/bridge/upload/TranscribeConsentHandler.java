abed2d67c6c6c7c5ac248569c4822e8cb0b3c6cf
Dwayne Jeng <dwayne.jeng@sagebase.org>
2019-08-02 16:48:45 -0700

    add study start time fallback logic

--- src/main/java/org/sagebionetworks/bridge/upload/TranscribeConsentHandler.java
+++ src/main/java/org/sagebionetworks/bridge/upload/TranscribeConsentHandler.java
@@ -2,7 +2,6 @@ package org.sagebionetworks.bridge.upload;
 
 import static org.sagebionetworks.bridge.BridgeUtils.mapSubstudyMemberships;
 
-import java.util.Map;
 import javax.annotation.Nonnull;
 
 import org.joda.time.DateTime;
@@ -16,15 +15,14 @@ import org.sagebionetworks.bridge.dao.AccountDao;
 import org.sagebionetworks.bridge.models.accounts.Account;
 import org.sagebionetworks.bridge.models.accounts.AccountId;
 import org.sagebionetworks.bridge.models.accounts.SharingScope;
-import org.sagebionetworks.bridge.models.activities.ActivityEventObjectType;
 import org.sagebionetworks.bridge.models.healthdata.HealthDataRecord;
-import org.sagebionetworks.bridge.services.ActivityEventService;
+import org.sagebionetworks.bridge.services.ParticipantService;
 import org.sagebionetworks.bridge.time.DateUtils;
 
 @Component
 public class TranscribeConsentHandler implements UploadValidationHandler {
     private AccountDao accountDao;
-    private ActivityEventService activityEventService;
+    private ParticipantService participantService;
 
     @Autowired
     public final void setAccountDao(AccountDao accountDao) {
@@ -32,14 +30,14 @@ public class TranscribeConsentHandler implements UploadValidationHandler {
     }
 
     @Autowired
-    public final void setActivityEventService(ActivityEventService activityEventService) {
-        this.activityEventService = activityEventService;
+    public final void setParticipantService(ParticipantService participantService) {
+        this.participantService = participantService;
     }
 
     @Override
     public void handle(@Nonnull UploadValidationContext context) {
         HealthDataRecord record = context.getHealthDataRecord();
-        
+
         AccountId accountId = AccountId.forHealthCode(context.getStudy().getIdentifier(), context.getHealthCode());
         Account account = accountDao.getAccount(accountId);
         if (account != null) {
@@ -49,22 +47,17 @@ public class TranscribeConsentHandler implements UploadValidationHandler {
             record.setUserDataGroups(account.getDataGroups());
             record.setUserSubstudyMemberships( mapSubstudyMemberships(account) );
 
-            // Calculate dayInStudy from activities_retrieved.
-            Map<String, DateTime> activityMap = activityEventService.getActivityEventMap(context.getHealthCode());
-            DateTime activitiesRetrievedDateTime = activityMap.get(ActivityEventObjectType.ACTIVITIES_RETRIEVED.name()
-                    .toLowerCase());
-            if (activitiesRetrievedDateTime != null) {
-                // Snap to a calendar date in the local time zone. For example, a participant has an
-                // activities_retrieved with calendar date 2019-07-24. Therefore, 2019-07-24 would be day 1, 2019-07-25
-                // would be day 2, etc.
-                LocalDate activitiesRetrievedLocalDate = activitiesRetrievedDateTime
-                        .withZone(BridgeConstants.LOCAL_TIME_ZONE).toLocalDate();
-                LocalDate todayLocalDate = DateUtils.getCurrentCalendarDateInLocalTime();
+            // Calculate dayInStudy.
+            // Snap to a calendar date in the local time zone. For example, a participant has an
+            // activities_retrieved with calendar date 2019-07-24. Therefore, 2019-07-24 would be day 1, 2019-07-25
+            // would be day 2, etc.
+            DateTime studyStartTime = participantService.getStudyStartTime(accountId);
+            LocalDate studyStartDate = studyStartTime.withZone(BridgeConstants.LOCAL_TIME_ZONE).toLocalDate();
+            LocalDate todayLocalDate = DateUtils.getCurrentCalendarDateInLocalTime();
 
-                // "dayInStudy" is 1-indexed. The first day of the study is day 1. Therefore, add 1.
-                int dayInStudy = Days.daysBetween(activitiesRetrievedLocalDate, todayLocalDate).getDays() + 1;
-                record.setDayInStudy(dayInStudy);
-            }
+            // "dayInStudy" is 1-indexed. The first day of the study is day 1. Therefore, add 1.
+            int dayInStudy = Days.daysBetween(studyStartDate, todayLocalDate).getDays() + 1;
+            record.setDayInStudy(dayInStudy);
         } else {
             // default sharing to NO_SHARING
             record.setUserSharingScope(SharingScope.NO_SHARING);
--- src/test/java/org/sagebionetworks/bridge/upload/TranscribeConsentHandlerTest.java
+++ src/test/java/org/sagebionetworks/bridge/upload/TranscribeConsentHandlerTest.java
@@ -1,6 +1,5 @@
 package org.sagebionetworks.bridge.upload;
 
-import static org.mockito.Mockito.any;
 import static org.mockito.Mockito.when;
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertNull;
@@ -9,7 +8,6 @@ import static org.testng.Assert.assertSame;
 import java.util.Map;
 import java.util.Set;
 
-import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Sets;
 
@@ -26,18 +24,21 @@ import org.testng.annotations.Test;
 import org.sagebionetworks.bridge.TestConstants;
 import org.sagebionetworks.bridge.dao.AccountDao;
 import org.sagebionetworks.bridge.models.accounts.Account;
+import org.sagebionetworks.bridge.models.accounts.AccountId;
 import org.sagebionetworks.bridge.models.accounts.SharingScope;
-import org.sagebionetworks.bridge.models.activities.ActivityEventObjectType;
 import org.sagebionetworks.bridge.models.healthdata.HealthDataRecord;
 import org.sagebionetworks.bridge.models.substudies.AccountSubstudy;
-import org.sagebionetworks.bridge.services.ActivityEventService;
+import org.sagebionetworks.bridge.services.ParticipantService;
 
 public class TranscribeConsentHandlerTest {
-    private static final DateTime ACTIVITIES_RETRIEVED = DateTime.parse("2019-07-21T22:28:31.648-0700");
     private static final long MOCK_NOW_MILLIS = DateTime.parse("2019-07-25T15:22:42.531-0700").getMillis();
+    private static final DateTime STUDY_START_TIME = DateTime.parse("2019-07-21T22:28:31.648-0700");
     private static final String TEST_HEALTHCODE = "test-healthcode";
     private static final String TEST_EXTERNAL_ID = "test-external-id";
     private static final Set<String> TEST_USER_GROUPS = ImmutableSet.of("test-group1","test-group2");
+
+    private static final AccountId ACCOUNT_ID = AccountId.forHealthCode(TestConstants.TEST_STUDY_IDENTIFIER,
+            TEST_HEALTHCODE);
 
     @Mock
     private AccountDao mockAccountDao;
@@ -46,7 +47,7 @@ public class TranscribeConsentHandlerTest {
     private Account mockAccount;
 
     @Mock
-    private ActivityEventService mockActivityEventService;
+    private ParticipantService participantService;
 
     @InjectMocks
     private TranscribeConsentHandler handler;
@@ -68,8 +69,9 @@ public class TranscribeConsentHandlerTest {
     public void beforeMethod() { 
         MockitoAnnotations.initMocks(this);
 
-        // By default, activity event service returns an empty map.
-        when(mockActivityEventService.getActivityEventMap(any())).thenReturn(ImmutableMap.of());
+        // Set up mocks.
+        when(mockAccountDao.getAccount(ACCOUNT_ID)).thenReturn(mockAccount);
+        when(participantService.getStudyStartTime(ACCOUNT_ID)).thenReturn(STUDY_START_TIME);
 
         // Set up input record and context. Handler expects Health Code and RecordBuilder.
         inputRecord = HealthDataRecord.create();
@@ -81,15 +83,16 @@ public class TranscribeConsentHandlerTest {
 
     @Test
     public void test() {
-        AccountSubstudy as1 = AccountSubstudy.create("studyId", "subA", "id");
-        AccountSubstudy as2 = AccountSubstudy.create("studyId", "subB", "id");
+        AccountSubstudy as1 = AccountSubstudy.create(TestConstants.TEST_STUDY_IDENTIFIER, "subA",
+                "id");
+        AccountSubstudy as2 = AccountSubstudy.create(TestConstants.TEST_STUDY_IDENTIFIER, "subB",
+                "id");
         as2.setExternalId("extB");
         
         when(mockAccount.getExternalId()).thenReturn(TEST_EXTERNAL_ID);
         when(mockAccount.getSharingScope()).thenReturn(SharingScope.SPONSORS_AND_PARTNERS);
         when(mockAccount.getDataGroups()).thenReturn(TEST_USER_GROUPS);
         when(mockAccount.getAccountSubstudies()).thenReturn(ImmutableSet.of(as1, as2));
-        when(mockAccountDao.getAccount(any())).thenReturn(mockAccount);
 
         handler.handle(context);
         HealthDataRecord outputRecord = context.getHealthDataRecord();
@@ -102,11 +105,14 @@ public class TranscribeConsentHandlerTest {
         Map<String,String> substudyMemberships = outputRecord.getUserSubstudyMemberships();
         assertEquals(substudyMemberships.get("subA"), "<none>");
         assertEquals(substudyMemberships.get("subB"), "extB");
+
+        assertEquals(outputRecord.getDayInStudy().intValue(), 5);
     }
 
     @Test
     public void testNoParticipantOptions() {
-        // account will return null for all values (nothing mocked)
+        // account is null
+        when(mockAccountDao.getAccount(ACCOUNT_ID)).thenReturn(null);
 
         handler.handle(context);
         HealthDataRecord outputRecord = context.getHealthDataRecord();
@@ -121,24 +127,10 @@ public class TranscribeConsentHandlerTest {
     @Test
     public void emptyStringSetConvertedCorrectly() {
         when(mockAccount.getDataGroups()).thenReturn(ImmutableSet.of());
-        when(mockAccountDao.getAccount(any())).thenReturn(mockAccount);
 
         handler.handle(context);
         HealthDataRecord outputRecord = context.getHealthDataRecord();
 
         assertNull(outputRecord.getUserDataGroups());
-    }
-
-    @Test
-    public void dayInStudy() {
-        // Mock activity event service to return ACTIVITIES_RETRIEVED.
-        when(mockActivityEventService.getActivityEventMap(TEST_HEALTHCODE)).thenReturn(ImmutableMap.of(
-                ActivityEventObjectType.ACTIVITIES_RETRIEVED.name().toLowerCase(), ACTIVITIES_RETRIEVED));
-        when(mockAccountDao.getAccount(any())).thenReturn(mockAccount);
-
-        // Execute. Just check daysInStudy.
-        handler.handle(context);
-        HealthDataRecord outputRecord = context.getHealthDataRecord();
-        assertEquals(outputRecord.getDayInStudy().intValue(), 5);
     }
 }
