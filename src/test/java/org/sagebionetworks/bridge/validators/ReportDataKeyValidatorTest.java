03a77f6171818cff7ee6783a25b9993ac2f1e449
Alx Dark <alx.dark@sagebase.org>
2020-04-29 12:17:03 -0700

    Slight update.

--- src/test/java/org/sagebionetworks/bridge/validators/ReportDataKeyValidatorTest.java
+++ src/test/java/org/sagebionetworks/bridge/validators/ReportDataKeyValidatorTest.java
@@ -66,7 +66,7 @@ public class ReportDataKeyValidatorTest {
     }
     
     @Test
-    public void appIdentifierMissing() {
+    public void appIdMissing() {
         test(() -> {
             new ReportDataKey.Builder()
                     .withIdentifier("foo")
