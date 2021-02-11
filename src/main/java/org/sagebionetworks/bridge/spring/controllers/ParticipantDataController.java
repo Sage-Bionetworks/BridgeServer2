75dea6b1ff748ad2576ad53c904062615417946d
Kelly Song <ksong@Kellys-MacBook-Air.local>
2021-02-10 18:27:30 -0800

    I'm super flabbergasted by deleteDataByIdentifier() right now. Even using service.deleteAllParticipantData(), which is the wrong logic for this endpoint, doesn't work. But it works for deleteAllParticipantDataForAdmin(). So I'm at a loss and will come back to this after going through other change requests

--- src/main/java/org/sagebionetworks/bridge/spring/controllers/ParticipantDataController.java
+++ src/main/java/org/sagebionetworks/bridge/spring/controllers/ParticipantDataController.java
@@ -74,6 +74,8 @@ public class ParticipantDataController extends BaseController {
     public StatusMessage deleteDataByIdentifier(@PathVariable String identifier) {
         UserSession session = getAuthenticatedAndConsentedSession();
 
+        //participantDataService.deleteAllParticipantData(session.getId()); //TODO just trying this bc this works in deleteAllParticipantDataForAdmin() but not here?!?!?
+
         participantDataService.deleteParticipantData(session.getId(), identifier);
 
         return new StatusMessage("Participant data record deleted.");
