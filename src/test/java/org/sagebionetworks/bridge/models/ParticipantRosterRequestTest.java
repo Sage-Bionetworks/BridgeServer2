92a05bd5a0328cfab742e1f21fa9afad7adbf383
Kelly Song <ksong@Kellys-MacBook-Air.local>
2021-04-21 10:04:34 -0700

    implement ParticipantRosterRequestTest

--- src/test/java/org/sagebionetworks/bridge/models/ParticipantRosterRequestTest.java
+++ src/test/java/org/sagebionetworks/bridge/models/ParticipantRosterRequestTest.java
@@ -1,7 +1,41 @@
 package org.sagebionetworks.bridge.models;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import org.sagebionetworks.bridge.json.BridgeObjectMapper;
+import org.testng.annotations.Test;
+
 import static org.testng.Assert.*;
 
 public class ParticipantRosterRequestTest {
+    private static final String PASSWORD = "P@ssword1";
+    private static final String STUDY_ID = "test-study-id";
 
+    @Test
+    public void builder() {
+        ParticipantRosterRequest request = new ParticipantRosterRequest.Builder().withPassword(PASSWORD)
+                .withStudyId(STUDY_ID).build();
+        assertEquals(request.getPassword(), PASSWORD);
+        assertEquals(request.getStudyId(), STUDY_ID);
+    }
+
+    @Test
+    public void serialize() throws JsonProcessingException {
+        // JSON
+        String jsonText = "{\n" +
+                "   \"password\":\"" + PASSWORD + "\",\n" +
+                "   \"studyId\":\"" + STUDY_ID + "\"\n" +
+                "}";
+
+        // Convert to POJO
+        ParticipantRosterRequest request = BridgeObjectMapper.get().readValue(jsonText, ParticipantRosterRequest.class);
+        assertEquals(request.getPassword(), PASSWORD);
+        assertEquals(request.getStudyId(), STUDY_ID);
+
+        // Convert back to JSON
+        JsonNode node = BridgeObjectMapper.get().convertValue(request, JsonNode.class);
+        assertEquals(node.size(), 3);
+        assertEquals(node.get("password").textValue(), PASSWORD);
+        assertEquals(node.get("studyId").textValue(), STUDY_ID);
+    }
 }
