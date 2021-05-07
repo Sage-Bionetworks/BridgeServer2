c63aaec0fa553b09329daa7713cdcd0564cb3246
Kelly Song <ksong@Kellys-MacBook-Air.local>
2021-05-07 13:18:41 -0700

    implement dao interface and dynamo objects

--- src/main/java/org/sagebionetworks/bridge/dao/HealthDataDocumentationDao.java
+++ src/main/java/org/sagebionetworks/bridge/dao/HealthDataDocumentationDao.java
@@ -1,2 +1,24 @@
-package org.sagebionetworks.bridge.dao;public interface HealthDataDocumentationDao {
+package org.sagebionetworks.bridge.dao;
+
+import org.sagebionetworks.bridge.json.BridgeTypeName;
+import org.sagebionetworks.bridge.models.ForwardCursorPagedResourceList;
+import org.sagebionetworks.bridge.models.HealthDataDocumentation;
+
+import javax.annotation.Nonnull;
+import java.util.Optional;
+
+/** DAO for health data documentation. */
+public interface HealthDataDocumentationDao {
+    /** Create or update health data documentation. Returns the created or updated documentation. */
+    HealthDataDocumentation createOrUpdateDocumentation(@Nonnull HealthDataDocumentation documentation);
+
+    /** Deletes all health data documentation for the given parentId. */
+    void deleteDocumentationForParentId(@Nonnull String parentId);
+
+    /** Retrieves the documentation for the given identifier. */
+    Optional<HealthDataDocumentation> getDocumentationById(@Nonnull String identifier);
+
+    /** Retrieves all documentation for the given parentId */
+    ForwardCursorPagedResourceList<HealthDataDocumentation> getDocumentationForParentId(@Nonnull String parentId,
+                                                                                        int pageSize, String offsetKey);
 }
--- src/main/java/org/sagebionetworks/bridge/dynamodb/DynamoHealthDataDocumentationDao.java
+++ src/main/java/org/sagebionetworks/bridge/dynamodb/DynamoHealthDataDocumentationDao.java
@@ -1,2 +1,109 @@
-package org.sagebionetworks.bridge.dynamodb;public class DynamoHealthDataDocumentationDao {
+package org.sagebionetworks.bridge.dynamodb;
+
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBQueryExpression;
+import com.amazonaws.services.dynamodbv2.model.AttributeValue;
+import com.amazonaws.services.dynamodbv2.model.ComparisonOperator;
+import com.amazonaws.services.dynamodbv2.model.Condition;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Iterables;
+import org.sagebionetworks.bridge.BridgeUtils;
+import org.sagebionetworks.bridge.dao.HealthDataDocumentationDao;
+import org.sagebionetworks.bridge.models.ForwardCursorPagedResourceList;
+import org.sagebionetworks.bridge.models.HealthDataDocumentation;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Resource;
+import java.util.List;
+import java.util.Optional;
+
+@Component
+public class DynamoHealthDataDocumentationDao implements HealthDataDocumentationDao {
+    private DynamoDBMapper mapper;
+
+    /** DynamoDB mapper for the HealthDataDocumentation table, configured by Spring. */
+    @Resource(name = "healthDataDbMapper")
+    public void setMapper(DynamoDBMapper mapper) {
+        this.mapper = mapper;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public HealthDataDocumentation createOrUpdateDocumentation(@Nonnull HealthDataDocumentation documentation) {
+        HealthDataDocumentation dynamoDocumentation = (DynamoHealthDataDocumentation) documentation;
+
+        if (dynamoDocumentation.getIdentifier() == null) {
+            // Documentation doesn't have ID assigned yet (new documentation). Create ID and assign it.
+            dynamoDocumentation.setIdentifier(BridgeUtils.generateGuid());
+        }
+
+        // Save to DynamoDB.
+        mapper.save(dynamoDocumentation);
+        return dynamoDocumentation;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public void deleteDocumentationForParentId(@Nonnull String parentId) {
+        // Query the records that we need to delete.
+        DynamoHealthDataDocumentation key = new DynamoHealthDataDocumentation();
+        key.setParentId(parentId);
+
+        DynamoDBQueryExpression<DynamoHealthDataDocumentation> query =
+                new DynamoDBQueryExpression<DynamoHealthDataDocumentation>().withHashKeyValues(key);
+        List<DynamoHealthDataDocumentation> docsToDelete = queryHelper(query);
+
+        // Batch delete.
+        if (!docsToDelete.isEmpty()) {
+            List<DynamoDBMapper.FailedBatch> failures = mapper.batchDelete(docsToDelete);
+            BridgeUtils.ifFailuresThrowException(failures);
+        }
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public Optional<HealthDataDocumentation> getDocumentationById(@Nonnull String identifier) {
+        return Optional.ofNullable(mapper.load(DynamoHealthDataDocumentation.class, identifier));
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public ForwardCursorPagedResourceList<HealthDataDocumentation> getDocumentationForParentId(@Nonnull String parentId,
+                                                                                    int pageSize, String offsetKey) {
+        // Create hash key.
+        DynamoHealthDataDocumentation key = new DynamoHealthDataDocumentation();
+        key.setParentId(parentId);
+        int pageSizeWithIndicatorRecord = pageSize + 1;
+
+        // Create query.
+        DynamoDBQueryExpression<DynamoHealthDataDocumentation> query =
+                new DynamoDBQueryExpression<DynamoHealthDataDocumentation>().withHashKeyValues(key)
+                        .withLimit(pageSizeWithIndicatorRecord);
+
+        // Add offset key condition to query.
+        if (offsetKey != null) {
+            Condition condition = new Condition().withComparisonOperator(ComparisonOperator.GE)
+                    .withAttributeValueList(new AttributeValue().withS(offsetKey));
+            query.withRangeKeyCondition("identifier", condition);
+        }
+
+        // Query.
+        List<DynamoHealthDataDocumentation> dynamoList = queryHelper(query);
+        List<HealthDataDocumentation> list = ImmutableList.copyOf(dynamoList);
+        list = list.size() <= pageSizeWithIndicatorRecord ? list : list.subList(0, pageSizeWithIndicatorRecord);
+
+        // Calculate nextOffsetKey, if present.
+        String nextOffsetKey = null;
+        if (list.size() == pageSizeWithIndicatorRecord) {
+            nextOffsetKey = Iterables.getLast(list).getIdentifier();
+        }
+
+        int limit = Math.min(list.size(), pageSize);
+        return new ForwardCursorPagedResourceList<>(list.subList(0, limit), nextOffsetKey);
+    }
+
+    List<DynamoHealthDataDocumentation> queryHelper(DynamoDBQueryExpression<DynamoHealthDataDocumentation> query) {
+        return mapper.query(DynamoHealthDataDocumentation.class, query);
+    }
 }
