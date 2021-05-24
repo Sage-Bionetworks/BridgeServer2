a6f2989e9e8cd026a761aa5d3f0162f8aca196a4
Kelly Song <ksong@Kellys-MacBook-Air.local>
2021-05-24 12:32:52 -0700

    complete first version model and endpoints

--- src/main/java/org/sagebionetworks/bridge/services/HealthDataDocumentationService.java
+++ src/main/java/org/sagebionetworks/bridge/services/HealthDataDocumentationService.java
@@ -1,5 +1,6 @@
 package org.sagebionetworks.bridge.services;
 
+import com.amazonaws.services.s3.AmazonS3Client;
 import org.sagebionetworks.bridge.BridgeConstants;
 import org.sagebionetworks.bridge.config.BridgeConfigFactory;
 import org.sagebionetworks.bridge.dao.HealthDataDocumentationDao;
@@ -16,6 +17,7 @@ import javax.annotation.Resource;
 import java.io.IOException;
 
 import static com.google.common.base.Preconditions.checkNotNull;
+import static org.sagebionetworks.bridge.BridgeConstants.API_DEFAULT_PAGE_SIZE;
 import static org.sagebionetworks.bridge.BridgeConstants.API_MAXIMUM_PAGE_SIZE;
 import static org.sagebionetworks.bridge.BridgeConstants.API_MINIMUM_PAGE_SIZE;
 
@@ -24,11 +26,17 @@ public class HealthDataDocumentationService {
     private static final String DOCUMENTATION_BUCKET = BridgeConfigFactory.getConfig().getProperty("healthdata.docs.bucket");
 
     private HealthDataDocumentationDao healthDataDocumentationDao;
+    private AmazonS3Client s3Client;
     private S3Helper s3Helper;
 
     @Autowired
     final void setHealthDataDocumentationDao(HealthDataDocumentationDao healthDataDocumentationDao) {
         this.healthDataDocumentationDao = healthDataDocumentationDao;
+    }
+
+    @Resource(name = "s3Client")
+    final void setS3Client(AmazonS3Client s3Client) {
+        this.s3Client = s3Client;
     }
 
     @Resource(name = "s3Helper")
@@ -53,15 +61,27 @@ public class HealthDataDocumentationService {
         if (documentation == null) {
             throw new EntityNotFoundException(HealthDataDocumentation.class);
         }
-        //TODO delete from S3
 
+        s3Client.deleteObject(DOCUMENTATION_BUCKET, documentation.getS3Key());
         healthDataDocumentationDao.deleteDocumentationForIdentifier(identifier, parentId);
     }
 
     /** Delete all health data documentation for the given parentId */
     public void deleteAllHealthDataDocumentation(String parentId) {
+        checkNotNull(parentId);
+
+        String offsetKey = null;
+        ForwardCursorPagedResourceList<HealthDataDocumentation> documentationList =
+                getAllHealthDataDocumentation(parentId, API_DEFAULT_PAGE_SIZE, offsetKey);
+
+        while ((offsetKey = documentationList.getNextPageOffsetKey()) != null) {
+            for (HealthDataDocumentation documentation : documentationList.getItems()) {
+                s3Client.deleteObject(DOCUMENTATION_BUCKET, documentation.getS3Key());
+            }
+            documentationList = getAllHealthDataDocumentation(parentId, API_DEFAULT_PAGE_SIZE, offsetKey);
+        }
+
         healthDataDocumentationDao.deleteDocumentationForParentId(parentId);
-        //TODO delete from S3
     }
 
     /** Get health data documentation for the given identifier. */
@@ -72,8 +92,6 @@ public class HealthDataDocumentationService {
             throw new EntityNotFoundException(HealthDataDocumentation.class);
         }
 
-        // TODO read from S3 and perhaps return byte[] instead of HealthDataDocumentation
-
         return documentation;
     }
 
@@ -82,8 +100,6 @@ public class HealthDataDocumentationService {
         if (pageSize < API_MINIMUM_PAGE_SIZE || pageSize > API_MAXIMUM_PAGE_SIZE) {
             throw new BadRequestException(BridgeConstants.PAGE_SIZE_ERROR);
         }
-
-        // TODO return list<byte[]> instead of HealthDataDocumentation
 
         return healthDataDocumentationDao.getDocumentationForParentId(parentId, pageSize, offsetKey);
     }
