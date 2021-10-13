296a793f5bfb7b08718fbe6948ddd65645b9b904
Dwayne Jeng <dwayne.jeng@sagebase.org>
2021-10-13 13:00:09 -0700

    Revert "Health Data Documentation"

--- src/test/java/org/sagebionetworks/bridge/models/ParticipantRosterRequestTest.java
+++ src/test/java/org/sagebionetworks/bridge/models/ParticipantRosterRequestTest.java
@@ -5,7 +5,7 @@ import com.fasterxml.jackson.databind.JsonNode;
 import org.sagebionetworks.bridge.json.BridgeObjectMapper;
 import org.testng.annotations.Test;
 
-import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.*;
 
 public class ParticipantRosterRequestTest {
     private static final String PASSWORD = "P@ssword1";
